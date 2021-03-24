5d51a989b33894ecd65e223b09afcc6e1d1feb8b
cjswo-duddn <cjswoudddn@naver.com>
2021-03-24 16:01:41 +0900

    samesite

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import axios from 'axios';
+
+
+
 function App() {
+  const handleClick = () =>{
+
+    const TEMPLATE_BASE_URL = 'http://localhost:8080/login';
+
+    axios({
+      method: 'post',
+      withCredentials: true,
+      url: TEMPLATE_BASE_URL,
+    })
+    .then((res)=>{
+      alert(res.data);
+    })
+    .catch((err)=>{
+      alert("Error")
+    });
+  }
   return (
     <>
-      Hello, World
+      <button onClick={handleClick}>button</button>
     </>
   );
 }
